3e648b2e7d408165d8dc1d6ca9235a6d80bf5053
ming liu <mingliuboy@gmail.com>
2016-03-01 10:26:44 +0800

    A test example

---
+++ hello-world/hello-world/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  hello-world
+//
+//  Created by Carl on 16/3/1.
+//  Copyright © 2016年 Carl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("hello world")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
