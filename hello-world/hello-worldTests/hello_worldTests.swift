8a7dd63da21de06207081c2f8c2d3e944f68fa8c
liu ming <mingliuboy@gmail.com>
2017-09-20 16:14:48 +0800

    add test XCTest Func

--- hello-world/hello-worldTests/hello_worldTests.swift
+++ hello-world/hello-worldTests/hello_worldTests.swift
@@ -24,6 +24,7 @@ class hello_worldTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        print("this a test hello-world......")
     }
     
     func testPerformanceExample() {
--- script/hello-worldBuild.sh
+++ script/hello-worldBuild.sh
@@ -1,4 +1,5 @@
 # build hello-world project
 
 cd hello-world
-xcodebuild -project hello-world.xcodeproj -scheme hello-world -configuration Debug -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO clean build
+xcodebuild -project hello-world.xcodeproj -scheme hello-worldTests -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 7,OS=10.3.1' test
+# xcodebuild -project hello-world.xcodeproj -scheme hello-world -configuration Debug -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO clean build
