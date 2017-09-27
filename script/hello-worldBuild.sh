75db189a2b3d8ede77afe12d34992bb7a056280c
liu ming <mingliuboy@gmail.com>
2017-09-27 14:10:02 +0800

    change helloworld 1

--- script/hello-worldBuild.sh
+++ script/hello-worldBuild.sh
@@ -1,5 +1,8 @@
 # build hello-world project
 
-cd hello-world
-xcodebuild -project hello-world.xcodeproj -scheme hello-worldTests -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 7,OS=10.3.1' test
-# xcodebuild -project hello-world.xcodeproj -scheme hello-world -configuration Debug -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO clean build
+# cd hello-world
+# xcodebuild -project hello-world.xcodeproj -scheme hello-worldTests -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 7,OS=10.3.1' test
+xcodebuild -project hello-world/hello-world.xcodeproj -scheme "hello-world" -configuration Debug -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6,OS=latest'
+# xcodebuild test -project URLHelpers/URLHelper.xcodeproj -scheme "URLHelper" -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6,OS=latest'
+
+
