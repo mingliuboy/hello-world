ebbad6d039219ef9d32936a953cf6ae6271c3d62
liu ming <mingliuboy@gmail.com>
2017-09-28 10:19:18 +0800

    change travis config 5

--- script/hello-worldBuild.sh
+++ script/hello-worldBuild.sh
@@ -2,7 +2,7 @@
 
 # cd hello-world
 # xcodebuild -project hello-world.xcodeproj -scheme hello-worldTests -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 7,OS=10.3.1' test
-xcodebuild clean build test -project hello-world/hello-world.xcodeproj -scheme "hello-world" -configuration Debug -sdk iphonesimulator
+xcodebuild test -project hello-world/hello-world.xcodeproj -scheme "hello-world" -configuration Debug -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone X,OS=11.0'
 
 # xcodebuild test -project URLHelpers/URLHelper.xcodeproj -scheme "URLHelper" -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6,OS=latest'
 
