b236926a753f482de69e78c9df72ebe2cfcd490b
MingLiu <mingliuboy@gmail.com>
2020-03-26 18:16:30 +0800

    Create reportScript.py

---
+++ hello-world/reportScript.py
@@ -0,0 +1,14 @@
+#coding=utf-8                            
+import os                                
+import datetime
+import time
+import re
+import math,random
+import sys
+
+
+print (sys.path)
+filePath=sys.argv[1]
+print("=====aaaa=====")
+print(filePath)
+print("=====bbbb=====")
